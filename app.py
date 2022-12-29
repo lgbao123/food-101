3dc27a32e85b555e90d9b5694a968f2cf01faf90
lgbao123 <luugiabao2609@gmail.com>
2022-12-29 16:21:01 +0700

    Fix bugs

--- app.py
+++ app.py
@@ -4,12 +4,13 @@ import pandas as pd
 import numpy as np
 from streamlit_option_menu import option_menu
 from PIL import Image
-from utils import load_and_prep, get_classes,returnModel
+from utils import load_and_prep, get_classes, returnModel
 import matplotlib.pyplot as plt
 # import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
-@st.cache(suppress_st_warning=True)
+import time
+# @st.cache(suppress_st_warning=True)
 def predicting(image, model):
     image = load_and_prep(image)
     image = tf.cast(tf.expand_dims(image, axis=0), tf.int16)
@@ -27,38 +28,50 @@ def predicting(image, model):
     df = df.sort_values('Scores')
     return pred_class, pred_conf, df
 
+
 class_names = get_classes()
+
+
+# @st.cache(suppress_st_warning=True)
+def prepareModel():
+    # global model
+    model = tf.keras.models.load_model("./models/FinalModel.hdf5")
+    st.session_state['model'] = model
 
 ### PLot function  ###
 
+
 def plotFoodInfo(namefood):
-    foodinfo =nutritiondf[nutritiondf['name']==namefood].loc[:,['protein','calcium','fat','carbohydrates','vitamins']]
+    foodinfo = nutritiondf[nutritiondf['name'] == namefood].loc[:, [
+        'protein', 'calcium', 'fat', 'carbohydrates', 'vitamins']]
     labels = foodinfo.columns.to_numpy()
-    values =foodinfo.values[0]
+    values = foodinfo.values[0]
     # Create subplots: use 'domain' type for Pie subplot
-    fig = make_subplots(rows=1, cols=2, specs=[[{'type':'domain'}, {'type':'domain'}]])
+    fig = make_subplots(rows=1, cols=2, specs=[
+                        [{'type': 'domain'}, {'type': 'domain'}]])
     fig.add_trace(go.Pie(labels=labels, values=values))
 
     fig.update_layout(
-        title_text="Nutrition Facts of "+namefood+ " (100g)",
-        margin =dict(l=3,r=3),
+        title_text="Nutrition Facts of "+namefood + " (100g)",
+        margin=dict(l=3, r=3),
         font=dict(
             # family="Courier New, monospace",
             size=20
             # color="RebeccaPurple"
         ),
         xaxis=go.layout.XAxis(
-        title=go.layout.xaxis.Title(
-            text="Fruits<br><sup>Fruit sales in the month of January</sup>"
+            title=go.layout.xaxis.Title(
+                text="Fruits<br><sup>Fruit sales in the month of January</sup>"
             )
         )
-        )
+    )
     st.write(fig)
     urlname = namefood.replace(" ", "-")
-    st.write(f"You can check out this [link](https://www.nutritionix.com/food/{urlname})")
+    st.write(
+        f"You can check out this [link](https://www.nutritionix.com/food/{urlname})")
 
 
-def plotPrediction(labels ,values ):
+def plotPrediction(labels, values):
     y_saving = values
     x = labels
 
@@ -81,7 +94,7 @@ def plotPrediction(labels ,values ):
             showline=False,
             showticklabels=True,
             # domain=[0, 0.85],
-            tickfont = dict(size=15),
+            tickfont=dict(size=15),
         ),
         xaxis_title="Percentage of prediction",
         paper_bgcolor='rgb(248, 248, 255)',
@@ -94,16 +107,17 @@ def plotPrediction(labels ,values ):
     y_s = np.round(y_saving, decimals=2)
 
     # Adding labels
-    for  yd, xd in zip( y_s, x):
+    for yd, xd in zip(y_s, x):
         annotations.append(dict(xref='x1', yref='y1',
                                 y=xd, x=yd + 6,
                                 text=str(yd) + '%',
                                 font=dict(family='Arial', size=16,
-                                        color='rgb(50, 171, 96)'),
+                                          color='rgb(50, 171, 96)'),
                                 showarrow=False))
 
     fig.update_layout(annotations=annotations)
     st.write(fig)
+
 
 def plotHistory(history_load):
     loss = history_load['loss']
@@ -112,52 +126,53 @@ def plotHistory(history_load):
     accuracy = history_load['accuracy']
     val_accuracy = history_load['val_accuracy']
 
-    epochs = np.arange(0,len(history_load['loss']))
+    epochs = np.arange(0, len(history_load['loss']))
 
-    fig = make_subplots(rows=1, cols=2,subplot_titles = ('Accuracy', 'Loss'))
+    fig = make_subplots(rows=1, cols=2, subplot_titles=('Accuracy', 'Loss'))
     fig.add_trace(
-        go.Scatter(x=epochs, y=accuracy,name='accuracy'),
+        go.Scatter(x=epochs, y=accuracy, name='accuracy'),
         row=1, col=1
     )
 
     fig.add_trace(
-        go.Scatter(x=epochs, y=val_accuracy,name='val_accuracy'),
+        go.Scatter(x=epochs, y=val_accuracy, name='val_accuracy'),
         row=1, col=1
     )
 
     fig.add_trace(
-        go.Scatter(x=epochs, y=loss,name='loss'),
+        go.Scatter(x=epochs, y=loss, name='loss'),
         row=1, col=2
     )
 
     fig.add_trace(
-        go.Scatter(x=epochs, y=val_loss,name='val_loss'),
+        go.Scatter(x=epochs, y=val_loss, name='val_loss'),
         row=1, col=2
     )
 
-    fig.update_layout(  height=600, width=800, 
-                        # title_text="Evaluate the model",
-                        hoverlabel = dict(font=dict(color='white')),
-                        yaxis=dict(
-                            domain=[0, 0.75],
-                        ),
-                        yaxis2=dict(
-                            domain=[0, 0.75],
-                        ),
-                        legend=dict(x=0.029, y=1.3,font_size = 16),
-                        font=dict(size=20)
-                    )
-    fig.update_annotations(yshift=-70,font = dict(size = 20))
+    fig.update_layout(height=600, width=800,
+                      # title_text="Evaluate the model",
+                      hoverlabel=dict(font=dict(color='white')),
+                      yaxis=dict(
+                          domain=[0, 0.75],
+                      ),
+                      yaxis2=dict(
+                          domain=[0, 0.75],
+                      ),
+                      legend=dict(x=0.029, y=1.3, font_size=16),
+                      font=dict(size=20)
+                      )
+    fig.update_annotations(yshift=-70, font=dict(size=20))
 
     # Update xaxis properties
     fig.update_xaxes(title_text="Epochs", row=1, col=1)
     fig.update_xaxes(title_text="Epochs", row=1, col=2)
     return fig
 
+
 def plotF1(class_f1_scores):
 
     # class_f1_scores=np.load('./history/score_101.npy',allow_pickle='TRUE').item()
-    report_df = pd.DataFrame(class_f1_scores, index = ['f1-scores']).T
+    report_df = pd.DataFrame(class_f1_scores, index=['f1-scores']).T
     report_df = report_df.sort_values("f1-scores", ascending=False)
     class_names = report_df.index.values.tolist()
     fig, ax = plt.subplots(figsize=(12, 25))
@@ -167,14 +182,14 @@ def plotF1(class_f1_scores):
     ax.set_yticklabels(class_names)
     ax.set_xlabel("f1-score")
     ax.set_title("F1-Scores for 101 Different Classes")
-    ax.invert_yaxis(); # reverse the order
+    ax.invert_yaxis()  # reverse the order
     return fig
 
-###------------------------------
+# ------------------------------
+
 
 st.set_page_config(page_title="Food Vision",
                    page_icon="🍔")
-
 
 
 #### SideBar ####
@@ -205,19 +220,25 @@ It can identify over 100 different food classes
 
 
 #### Main Body ####
-
-
-
-
+@st.cache(allow_output_mutation=True)
+def cached_model():
+    # URL = "https://github.com/hasibzunair/cxr-predictor/releases/latest/download/CheXpert_DenseNet121_res224.h5"
+    # URL = "https://github.com/lgbao123/food-101/raw/main/models/FinalModel.h5"
+    # weights_path = tf.keras.utils.get_file(
+    #     "FinalModel.h5",
+    #     URL)
+    model = tf.keras.models.load_model("./models/FinalModel.hdf5", compile=False)
+    return model
+model = cached_model()
 if selected == "Home":
-    model = tf.keras.models.load_model("./models/FinalModel.hdf5")
+
+
     nutritiondf = pd.read_csv("nutrition101.csv")
     st.title("Food Vision 🍔📷")
     st.header("Identify what's in your food photos!")
 
     file = st.file_uploader(label="Upload an image of food.",
                             type=["jpg", "jpeg", "png"])
-
 
     if not file:
         st.warning("Please upload an image")
@@ -226,29 +247,35 @@ if selected == "Home":
     else:
         image = file.read()
         st.image(image, use_column_width=True)
-        pred_button = st.button("Predict")
+        if model:
+            pred_button = st.button("Predict")
 
-    if pred_button:
-        pred_class, pred_conf, df = predicting(image, model)
-        st.success(f'Prediction : {pred_class}')
-        st.success(f'Confidence : {pred_conf*100:.2f}%')
-        # st.write(alt.Chart(df).mark_bar().encode(
-        #     x='Scores',
-        #     y=alt.X('Top 5 Predictions', sort=None),
-        #     color=alt.Color("color", scale=None),
-        #     text='F1 Scores'
-        # ).properties(width=600, height=400))
-        pred_class = pred_class.replace("_", " ")
-        st.write(df[['Top 5 Predictions','Scores']].sort_values('Scores', ascending= False))
-        plotPrediction(df['Top 5 Predictions'].to_numpy(),df['Scores'].to_numpy())
-        plotFoodInfo(pred_class)
+            if pred_button:
+                start_time = time.time()
+                pred_class, pred_conf, df = predicting(image, model)
+                
+                score_list =df['Scores'].to_list()
+                score_list = sorted(score_list, reverse=True)
+                check = all([score_list[0]>=5,score_list[1]>=5,score_list[2]>= 3])
+                if check:
+                    st.warning("It's not food or not in database ")
+                else:    
+                    st.success(f'Prediction : {pred_class}')
+                    st.success(f'Confidence : {pred_conf*100:.2f}%')
+                    st.write("Took {} seconds to run.".format(
+                        round(time.time() - start_time, 3)))
+                    pred_class = pred_class.replace("_", " ")
+                    st.write(df[['Top 5 Predictions', 'Scores']].sort_values(
+                        'Scores', ascending=False))
+                    
+                    plotPrediction(df['Top 5 Predictions'].to_numpy(),
+                                df['Scores'].to_numpy())
+                    plotFoodInfo(pred_class)
 if selected == "Projects":
     st.title(f"Multiclass Classification using TensorFlow 2.0 on Food-101 Dataset")
 
-
-
     st.markdown(
-    """
+        """
     The Food-101 Data Set :
     - This dataset consists of 101 food categories, with 101'000 images
     - Each type of food has 750 training samples and 250 test samples
@@ -263,10 +290,10 @@ if selected == "Projects":
     fit_2_image = Image.open('./extras/2_fit.png')
     fit_10_image = Image.open('./extras/10_fit.png')
     fit_101_image = Image.open('./extras/101_fit.png')
-    st.image([image],use_column_width=True)
+    st.image([image], use_column_width=True)
     # st.image([image1,image2])
     st.markdown(
-    """
+        """
     Preprocessing data :
     - Load the image and convert into numpy array
     - Resize the image (to the same size our model was trained on)
@@ -285,16 +312,18 @@ if selected == "Projects":
     #     st.image([image3],use_column_width=True)
     st.image([pre_image])
     model_selected = st.selectbox("Select Your Model: ",
-                        ['2 Classes', '10 Classes', '101 Classes (final model)'])
-    
+                                  ['2 Classes', '10 Classes', '101 Classes (final model)'])
+
     if model_selected == '2 Classes':
-            
+
         model_1 = returnModel()
         st.header("Summary model : ")
         model_1.summary(print_fn=lambda x: st.text(x))
         st.image([fit_2_image])
+        st.success("Total running time for training process: ~41 s (97% accuracy train - 88% accuracy in test) ")
         st.header("Evaluate the model : ")
-        history_load=np.load('./history/history_2.npy',allow_pickle='TRUE').item()
+        history_load = np.load('./history/history_2.npy',
+                               allow_pickle='TRUE').item()
         fig = plotHistory(history_load)
         st.write(fig)
     if model_selected == '10 Classes':
@@ -302,8 +331,10 @@ if selected == "Projects":
         st.header("Summary model : ")
         model_1.summary(print_fn=lambda x: st.text(x))
         st.image([fit_10_image])
+        st.success("Total running time for training process: ~20 m (88% accuracy train - 92% accuracy in test) ")
         st.header("Evaluate the model : ")
-        history_load=np.load('./history/history_10.npy',allow_pickle='TRUE').item()
+        history_load = np.load('./history/history_10.npy',
+                               allow_pickle='TRUE').item()
         fig = plotHistory(history_load)
         st.write(fig)
     if model_selected == '101 Classes (final model)':
@@ -311,12 +342,14 @@ if selected == "Projects":
         st.header("Summary model : ")
         model_1.summary(print_fn=lambda x: st.text(x))
         st.image([fit_101_image])
+        st.success("Total running time for training process: ~140 m (97% accuracy train - 85% accuracy in test)")
         st.header("Evaluate the model : ")
-        history_load=np.load('./history/history_101.npy',allow_pickle='TRUE').item()
+        history_load = np.load('./history/history_101.npy',
+                               allow_pickle='TRUE').item()
         fig = plotHistory(history_load)
         st.write(fig)
         st.write("F1 score of each class :")
-        class_f1_scores=np.load('./history/score_101.npy',allow_pickle='TRUE').item()
+        class_f1_scores = np.load(
+            './history/score_101.npy', allow_pickle='TRUE').item()
         fig1 = plotF1(class_f1_scores)
         st.pyplot(fig1)
-
